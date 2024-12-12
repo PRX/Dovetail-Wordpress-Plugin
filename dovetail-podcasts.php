977308fc1e38a1f6c0f398cbeb8bdc22c31d3c0b
Rick Peterman <rick.peterman@prx.org>
2024-12-12 16:53:23 -0600

    chore: add init file

---
+++ dovetail-podcasts.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Dovetail Podcasts
+ * Plugin URI: https://github.com/PRX/Dovetail-Wordpress-Plugin
+ * GitHub Plugin URI: https://github.com/PRX/Dovetail-Wordpress-Plugin
+ * Description: Dovetail Podcasts publishing integration.
+ * Author: PRX
+ * Author URI: http://prx.org
+ * Version: 0.0.0
+ * Text Domain: dovetail-podcasts
+ * License: GPL-3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package DovetailPodcasts
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
