594f433fac639b6263e537defc3f8ede7269d69f
Rick Peterman <rick.peterman@prx.org>
2026-05-19 11:09:11 -0500

    fix(workflows): version validation pattern
    - validate should allow a suffix w/ multiple word character or periods after a dash

--- scripts/utils/version.js
+++ scripts/utils/version.js
@@ -3,7 +3,9 @@
  */
 
 function validateVersion(version) {
-  return typeof version !== "string" && /^\d+\.\d+\.\d+(-[\w.])?/.test(version);
+  return (
+    typeof version !== "string" && /^\d+\.\d+\.\d+(-[\w.]+)?/.test(version)
+  );
 }
 
 // Export the utility functions
